ba7e1b80f70f0c72acbf22a2007803ee151e882e
zamaltha <akmalarts99@gmail.com>
2024-11-28 13:51:23 +0700

    Add files via upload
    untuk latihannya, di dom_selection

---
+++ script.js
@@ -0,0 +1,26 @@
+const header1 = document.getElementById("h1")           // element node
+const p = document.getElementsByTagName("p")            // html collection
+const a = document.getElementsByTagName("a")
+const list = document.getElementsByClassName("list")    // html collection 
+
+header1.innerHTML = "Belajar HTML dan JavaScript"
+container1.style.backgroundColor = "Teal"
+container2.style.backgroundColor = "Teal"
+p[0].style.color = "Yellow"
+p[1].style.color = "Yellow"
+a[0].style.color = "Yellow"
+
+list[0].style.color = "Yellow"
+list[0].style.fontFamily = "Bebas Neue"
+list[0].style.fontSize = "18px"
+list[0].style.backgroundColor = "Red"
+
+list[1].style.color = "Yellow"
+list[1].style.fontFamily = "Bebas Neue"
+list[1].style.fontSize = "18px"
+list[1].style.backgroundColor = "Red"
+
+list[2].style.color = "Yellow"
+list[2].style.fontFamily = "Bebas Neue"
+list[2].style.fontSize = "18px"
+list[2].style.backgroundColor = "Red"
